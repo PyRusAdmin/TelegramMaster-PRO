5795d0ba49792ed5a5bc3dd2598a2230181659e2
pyadminru <zh.vitaliy92@yandex.ru>
2025-11-08 22:19:39 +0300

    Рефакторинг кода проекта.

--- src/features/account/creating.py
+++ src/features/account/creating.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-import os
-import os.path
-import shutil
-
 import flet as ft
 from loguru import logger
+from telethon import TelegramClient
 from telethon import functions
 
 from src.core.configs import BUTTON_HEIGHT, WIDTH_WIDE_BUTTON
-from src.core.database.account import getting_account
+from src.core.database.account import getting_account, get_account_list
 from src.core.utils import Utils
 from src.features.account.connect import TGConnect
-from src.gui.gui_elements import GUIProgram
 from src.gui.gui import AppLogger, list_view
+from src.gui.gui_elements import GUIProgram
 from src.gui.notification import show_notification
 from src.locales.translations_loader import translations
 
@@ -29,45 +26,45 @@ class CreatingGroupsAndChats:
         self.utils = Utils(page=page)
         self.gui_program = GUIProgram()
         self.session_string = getting_account()  # Получаем строку сессии из файла базы данных
+        self.account_data = get_account_list()  # Получаем список аккаунтов из базы данных
 
     async def creating_groups_and_chats(self) -> None:
         """
         Создание групп (чатов) в автоматическом режиме
         """
-        selected_sessions = []  # Список для хранения выбранных session файлов
-        selected_files = ft.Text(value=translations["ru"]["notifications"]["files_not_selected"], selectable=True)
         self.page.controls.append(list_view)  # добавляем ListView на страницу для отображения логов 📝
         self.page.update()  # обновляем страницу, чтобы сразу показать ListView 🔄
+
+        # Создаём опции: текст — номер, ключ — session_string
+        account_options = [
+            ft.DropdownOption(text=phone, key=session_str)
+            for phone, session_str in self.account_data
+        ]
+        # Создаем выпадающий список с названиями групп
+        account_drop_down_list = ft.Dropdown(
+            label="📂 Выберите аккаунт",  # ✅ Название выпадающего списка
+            width=WIDTH_WIDE_BUTTON,  # ✅ Ширина выпадающего списка
+            options=account_options,  # ✅ Опции выпадающего списка
+            autofocus=True  # ✅ Автозаполнение
+        )
 
         async def add_items(_):
             """
             🚀 Запускает процесс создания групп и отображает статус в интерфейсе.
             """
+            selected_account = account_drop_down_list.value  # ← Получаем key выбранного аккаунта
+
             start = await self.app_logger.start_time()
             self.page.update()
 
-            if not selected_sessions:
-                await self.app_logger.log_and_display(
-                    message=translations["ru"]["errors"]["files_not_selected_warning"])
-
-                if not session_string:
-                    await self.app_logger.log_and_display(message=translations["ru"]["errors"]["no_session_files"])
-                    self.page.update()
-                    return
-            else:
-                session_files = selected_sessions
-                await self.app_logger.log_and_display(message=translations["ru"]["notifications"]["start_creating"])
             try:
-                for session_name in session_files:
-                    # Извлекаем только имя файла без расширения
-                    session_name = os.path.splitext(os.path.basename(session_name))[0]
-                    client = await self.connect.client_connect_string_session(session_name=session_name)
-                    await self.connect.getting_account_data(client=client)
-                    await client(functions.channels.CreateChannelRequest(title='My awesome title',
-                                                                         about='Description for your group',
-                                                                         megagroup=True))
-                    await self.app_logger.log_and_display(
-                        message=translations["ru"]["notifications"]["notification_creating"])
+                client: TelegramClient = await self.connect.client_connect_string_session(session_name=selected_account)
+                await self.connect.getting_account_data(client=client)
+                await client(functions.channels.CreateChannelRequest(title='My awesome title',
+                                                                     about='Description for your group',
+                                                                     megagroup=True))
+                await self.app_logger.log_and_display(
+                    message=translations["ru"]["notifications"]["notification_creating"])
             except TypeError:
                 pass
             except Exception as error:
@@ -75,35 +72,6 @@ class CreatingGroupsAndChats:
             await self.app_logger.end_time(start=start)
             await show_notification(page=self.page,
                                     message="🔚 Создания групп (чатов)")  # Выводим уведомление пользователю
-
-        async def btn_click(e: ft.FilePickerResultEvent) -> None:
-            if e.files:
-                selected_sessions.clear()
-                for file in e.files:
-                    file_name = file.name
-                    file_path = file.path
-                    if file_name.endswith(".session"):
-                        target_folder = path_accounts_folder
-                        target_path = os.path.join(target_folder, file_name)
-                        if not os.path.exists(target_path) or file_path != os.path.abspath(target_path):
-                            os.makedirs(target_folder, exist_ok=True)
-                            shutil.copy(file_path, target_path)
-                        selected_sessions.append(target_path)
-                    else:
-                        selected_files.value = f"Файл {file_name} не является session файлом. Выберите только .session файлы."
-                        selected_files.update()
-                        return
-                selected_files.value = f"Выбраны session файлы: {', '.join([os.path.basename(s) for s in selected_sessions])}"
-                selected_files.update()
-            else:
-                selected_files.value = "Выбор файлов отменен"
-                selected_files.update()
-
-            self.page.update()
-
-        # Кнопка выбора session файлов
-        pick_files_dialog = ft.FilePicker(on_result=btn_click)
-        self.page.overlay.append(pick_files_dialog)
 
         # Добавляем элементы интерфейса на страницу
         self.page.views.append(ft.View("/creating_groups_and_chats_menu",
@@ -114,14 +82,11 @@ class CreatingGroupsAndChats:
                                                 foreground=ft.Paint(gradient=ft.PaintLinearGradient((0, 20), (150, 20),
                                                                                                     [ft.Colors.PINK,
                                                                                                      ft.Colors.PURPLE]))))]),
-                                        list_view, selected_files,
-                                        ft.ElevatedButton(width=WIDTH_WIDE_BUTTON, height=BUTTON_HEIGHT,
-                                                          text=translations["ru"]["create_groups_menu"][
-                                                              "choose_session_files"],
-                                                          on_click=lambda _: pick_files_dialog.pick_files(
-                                                              allow_multiple=True)),
+                                        list_view,
+                                        account_drop_down_list,
                                         ft.ElevatedButton(width=WIDTH_WIDE_BUTTON, height=BUTTON_HEIGHT,
                                                           text=translations["ru"]["buttons"]["start"],
                                                           on_click=add_items),
                                         ]))
         self.page.update()
+# 144
