dcae9a7367deb53d08917a4a2e6b7f050cfa6ea2
PyRusAdmin <zh.vitaliy92@yandex.ru>
2025-11-27 00:45:40 +0300

    Формирование документации

--- docs/app.py
+++ docs/app.py
@@ -11,7 +11,9 @@ from fastapi.templating import Jinja2Templates
 from loguru import logger
 
 # Импорт необходимых переменных и функций
-from src.core.config.configs import PROGRAM_NAME, PROGRAM_VERSION, DATE_OF_PROGRAM_CHANGE, path_folder_database
+from src.core.config.configs import (
+    PROGRAM_NAME, PROGRAM_VERSION, DATE_OF_PROGRAM_CHANGE, path_folder_database
+)
 from src.locales.translations_loader import translations
 
 app = FastAPI()
