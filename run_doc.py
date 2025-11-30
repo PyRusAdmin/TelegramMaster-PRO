354727234fe448081bb48e66184cafc828a8d562
pyadminru <zh.vitaliy92@yandex.ru>
2025-11-30 19:04:20 +0300

    Формирование документации

--- run_doc.py
+++ run_doc.py
@@ -2,5 +2,9 @@
 
 """Запуск документации"""
 from docs.app import start_app
+from loguru import logger
 
-start_app()
+logger.info("Запуск документации")
+
+if __name__ == "__main__":
+    start_app()
