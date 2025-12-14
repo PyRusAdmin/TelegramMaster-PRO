c1c395f821a5bd4e01b56305eb935e569f967951
PyRusAdmin <zh.vitaliy92@yandex.ru>
2025-12-14 20:51:45 +0300

    Формирование докстринг документации

--- src/locales/translations_loader.py
+++ src/locales/translations_loader.py
@@ -3,6 +3,11 @@ import yaml
 
 
 def load_translations():
+    """
+    Загружает переводы из YAML-файла.
+
+    :return: Словарь с переводами
+    """
     with open("src/locales/translations.yaml", "r", encoding="utf-8") as file:
         return yaml.safe_load(file)
 
