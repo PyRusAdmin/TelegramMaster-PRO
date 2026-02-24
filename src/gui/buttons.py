a42f0ac20819bfedc87f322b4039adcaf839d650
pyadminru <zh.vitaliy92@yandex.ru>
2026-02-25 00:39:20 +0300

    Рефакторинг кода

--- src/gui/buttons.py
+++ src/gui/buttons.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-import asyncio
-
 import flet as ft  # Импортируем библиотеку flet
 
-from src.core.configs import WIDTH_WIDE_BUTTON, BUTTON_HEIGHT, BUTTON_WIDTH
+from src.core.configs import WIDTH_WIDE_BUTTON, BUTTON_HEIGHT
 from src.gui.gui import list_view
 from src.gui.gui_elements import GUIProgram
 from src.locales.translations_loader import translations
